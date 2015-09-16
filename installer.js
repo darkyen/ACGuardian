b453b6a82bb14891d51ac7a1899b76e139d90753
Abhishek Hingnikar <abhishek@hingnikar.com>
2015-09-16 01:49:33 -0700

    Final changes now it really works

--- installer.js
+++ installer.js
@@ -3,17 +3,21 @@ var Service = windows.Service;
 
 var svc = new Service({
   name:'ACGuardian',
+  description: 'Service to protect you from having critical shutdown on power failure',
   script: require('path').join(__dirname, 'index.js')
 });
 
 function initService(){
 	svc.start();
 }
-
+svc.uninstall();
 windows.isAdminUser(function(isAdmin){
 	if( isAdmin ){
 		svc.on('install', initService);
 		svc.on('alreadyinstalled', initService);		
+		svc.on('start', console.log.bind(console, 'Service Started Hurrah !'));
+		svc.on('stop', console.log.bind(console, 'Service stoped boooah !'));
+		svc.on('error', console.error.bind(console, 'OMFG ERRAH!'));
 		svc.install();
 	}
 });
